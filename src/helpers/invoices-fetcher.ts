56b628cb2b5a4479993b26b84b0ee0bd8f3b01c8
dbritto-dev <dbritto.dev@gmail.com>
2022-06-13 21:00:33 -0500

    Update invoices fetcher helper

--- src/helpers/invoices-fetcher.ts
+++ src/helpers/invoices-fetcher.ts
@@ -2,5 +2,5 @@ import type { QueryFunctionContext } from "react-query";
 import { Invoice } from "models/invoices";
 import { unfetch } from "./unfetch";
 
-export const invoicesFetcher = (_: QueryFunctionContext) =>
-  unfetch<Invoice[]>("/api/invoices.json");
+export const invoicesFetcher = ({ signal }: QueryFunctionContext) =>
+  unfetch<Invoice[]>("/api/invoices.json", { signal });
