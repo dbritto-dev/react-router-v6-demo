5135ff4b8232fe3c8e1bb9ccb64bc8dd89817f0b
dbritto-dev <dbritto.dev@gmail.com>
2022-06-16 18:46:28 -0500

    Update app

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,5 @@
-import { Layout } from "./components/layout";
 import { Navigation } from "./navigation";
 
 export default function App() {
-  return (
-    <Layout>
-      <Navigation />
-    </Layout>
-  );
+  return <Navigation />;
 }
