2aacd2746e95047d4ee0cb1785c7449babfeb5f7
dbritto-dev <dbritto.dev@gmail.com>
2022-06-16 18:45:51 -0500

    Update navigation lazy import component to prefetch routes

--- src/_navigation.lazy-imports.tsx
+++ src/_navigation.lazy-imports.tsx
@@ -1,37 +1,124 @@
 import { lazy } from "react";
-import { Routes, Route, Outlet } from "react-router-dom";
-import { SmallSpinnit } from "components/small-spinnit";
+import { Routes, Route, Navigate } from "react-router-dom";
+import MainLayout from "routes/_layout";
 
-const Dashboard = lazy(() => import("routes/dashboard"));
-const Accounts = lazy(() => import("routes/accounts"));
-const Expenses = lazy(() => import("routes/expenses"));
-const Reports = lazy(() => import("routes/reports"));
-const Sales = lazy(() => import("routes/sales"));
-const SalesLayout = lazy(() => import("routes/sales/_layout"));
-const SalesCustomers = lazy(() => import("routes/sales/customers"));
-const SalesDeposits = lazy(() => import("routes/sales/deposits"));
-const SalesSubscriptions = lazy(() => import("routes/sales/subscriptions"));
-const SalesInvoices = lazy(() => import("routes/sales/invoices"));
-const SalesInvoicesLayout = lazy(() =>
-  import("routes/sales/invoices/_layout")
+const Dashboard = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "dashboard" */
+      /* webpackPrefetch: true */
+      "routes/dashboard"
+    )
 );
-const SalesInvoicesDetail = lazy(() =>
-  import("routes/sales/invoices/detail")
+const Accounts = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "accounts" */
+      /* webpackPrefetch: true */
+      "routes/accounts"
+    )
 );
-const Home = lazy(() => import("routes/home"));
-
-function SmallSpinnitLayoutRoute() {
-  return (
-    <SmallSpinnit>
-      <Outlet />
-    </SmallSpinnit>
-  );
-}
+const Expenses = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "expenses" */
+      /* webpackPrefetch: true */
+      "routes/expenses"
+    )
+);
+const Reports = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "reports" */
+      /* webpackPrefetch: true */
+      "routes/reports"
+    )
+);
+const Sales = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "sales" */
+      /* webpackPrefetch: true */
+      "routes/sales"
+    )
+);
+const SalesLayout = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "sales-layout" */
+      /* webpackPrefetch: true */
+      "routes/sales/_layout"
+    )
+);
+const SalesCustomers = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "sales-customers" */
+      /* webpackPreload: true */
+      "routes/sales/customers"
+    )
+);
+const SalesDeposits = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "sales-deposits" */
+      /* webpackPrefetch: true */
+      "routes/sales/deposits"
+    )
+);
+const SalesSubscriptions = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "sales-subscriptions" */
+      /* webpackPrefetch: true */
+      "routes/sales/subscriptions"
+    )
+);
+const SalesInvoices = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "sales-invoices" */
+      /* webpackPrefetch: true */
+      "routes/sales/invoices"
+    )
+);
+const SalesInvoicesLayout = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "sales-invoices-layout" */
+      /* webpackPrefetch: true */
+      "routes/sales/invoices/_layout"
+    )
+);
+const SalesInvoicesDetail = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "sales-invoices-detail" */
+      /* webpackPrefetch: true */
+      "routes/sales/invoices/detail"
+    )
+);
+const Home = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "home" */
+      /* webpackPrefetch: true */
+      "routes/home"
+    )
+);
+const NotFound = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "not-found" */
+      /* webpackPrefetch: true */
+      "routes/not-found"
+    )
+);
 
 export function Navigation() {
   return (
     <Routes>
-      <Route element={<SmallSpinnitLayoutRoute />}>
+      <Route element={<MainLayout />}>
         <Route element={<Home />} index />
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="accounts" element={<Accounts />} />
@@ -44,9 +131,12 @@ export function Navigation() {
           <Route path="customers" element={<SalesCustomers />} />
           <Route path="deposits" element={<SalesDeposits />} />
           <Route path="subscriptions" element={<SalesSubscriptions />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
         <Route path="expenses" element={<Expenses />} />
         <Route path="reports" element={<Reports />} />
+        <Route path="manifest.json" element={<Navigate to="." replace />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
