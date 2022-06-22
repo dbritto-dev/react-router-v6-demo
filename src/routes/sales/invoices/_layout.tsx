95d53ef1c2bc3bada3f422c57bbbe8289097b3a0
dbritto-dev <dbritto.dev@gmail.com>
2022-06-22 11:42:19 -0500

    Update sales invoices component

--- src/routes/sales/invoices/_layout.tsx
+++ src/routes/sales/invoices/_layout.tsx
@@ -1,86 +1,12 @@
-import { NavLink, Outlet } from "react-router-dom";
-import { useQuery, useQueryClient } from "react-query";
+import { Outlet } from "react-router-dom";
+import { useQuery } from "react-query";
 import { LabelText } from "components/label-text";
 import { SmallSpinnit } from "components/small-spinnit";
 import { invoicesFetcher } from "helpers/invoices-fetcher";
 import { getInvoiceDueStatus } from "helpers/get-invoice-due-status";
 import { getInvoiceTotal } from "helpers/get-invoice-total";
-import { invoiceFetcher } from "helpers/invoice-fetcher";
-
-function InvoicesInfo({
-  label,
-  amount,
-  right,
-}: {
-  label: string;
-  amount: number;
-  right?: boolean;
-}) {
-  return (
-    <div className={right ? "text-right" : ""}>
-      <LabelText>{label}</LabelText>
-      <div className="text-[length:18px] text-black">
-        ${amount.toLocaleString()}
-      </div>
-    </div>
-  );
-}
-
-function InvoiceList({ children }: { children: React.ReactNode }) {
-  const queryClient = useQueryClient();
-  const { data: invoiceListItems } = useQuery("invoices", invoicesFetcher, {
-    suspense: true,
-  });
-
-  if (invoiceListItems === undefined) return null;
-
-  return (
-    <div className="flex rounded-lg border border-gray-100">
-      <div className="w-1/2 border-r border-gray-100">
-        {invoiceListItems.map((invoice, index) => (
-          <NavLink
-            key={index}
-            to={invoice.id}
-            onMouseEnter={async () =>
-              void (await queryClient.prefetchQuery(
-                ["invoice", invoice.id],
-                invoiceFetcher(invoice.id),
-                { staleTime: 1000 * 2 }
-              ))
-            }
-            className={({ isActive }) =>
-              "block border-b border-gray-50 py-3 px-4 hover:bg-gray-50" +
-              " " +
-              (isActive ? "bg-gray-50" : "")
-            }
-          >
-            <div className="flex justify-between text-[length:14px] font-bold leading-6">
-              <div>{invoice.name}</div>
-              <div>${getInvoiceTotal(invoice).toLocaleString()}</div>
-            </div>
-            <div className="flex justify-between text-[length:12px] font-medium leading-4 text-gray-400">
-              <div>{invoice.number}</div>
-              <div
-                className={
-                  "uppercase" +
-                  " " +
-                  (getInvoiceDueStatus(invoice) === "paid"
-                    ? "text-green-400"
-                    : getInvoiceDueStatus(invoice) === "overdue"
-                    ? "text-red-400"
-                    : "")
-                }
-              >
-                {getInvoiceDueStatus(invoice)}
-              </div>
-            </div>
-          </NavLink>
-        ))}
-      </div>
-      <div className="w-1/2">{children}</div>
-    </div>
-  );
-}
+import { InvoicesInfo } from "components/invoices-info";
+import { InvoiceList } from "components/invoice-list";
 
 export default function Layout() {
   const { data: invoiceListItems } = useQuery("invoices", invoicesFetcher, {
