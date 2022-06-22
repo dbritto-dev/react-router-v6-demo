1b6b305b2caa90f06294ad2745106e09ec202805
dbritto-dev <dbritto.dev@gmail.com>
2022-06-22 12:17:19 -0500

    Update invoice fetcher helper

--- src/helpers/invoice-fetcher.ts
+++ src/helpers/invoice-fetcher.ts
@@ -1,8 +1,8 @@
-import type { QueryFunctionContext } from "react-query";
+import type { QueryFunction } from "react-query";
 import { Invoice } from "models/invoices";
 import { unfetch } from "./unfetch";
 
-export const invoiceFetcher = (invoiceId: string) => async ({
-  signal
-}: QueryFunctionContext) =>
-  unfetch<Invoice>(`/api/invoices/${invoiceId}.json`, { signal });
+export const invoiceFetcher =
+  (invoiceId: string): QueryFunction<Invoice> =>
+  async ({ signal }) =>
+    unfetch(`/api/invoices/${invoiceId}.json`, { signal });
