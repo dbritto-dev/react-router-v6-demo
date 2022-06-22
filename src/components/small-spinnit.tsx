62fd40761a04a429711dbc0e8b15990079dfd588
dbritto-dev <dbritto.dev@gmail.com>
2022-06-22 11:40:46 -0500

    Update small spinnit component

--- src/components/small-spinnit.tsx
+++ src/components/small-spinnit.tsx
@@ -2,7 +2,11 @@ import { Suspense } from "react";
 import type { ReactNode } from "react";
 import { Spinner } from "./spinner";
 
-export const SmallSpinnit = ({ children }: { children: ReactNode }) => (
+export interface SmallSpinnitProps {
+  children?: ReactNode;
+}
+
+export const SmallSpinnit = ({ children }: SmallSpinnitProps) => (
   <Suspense fallback={<Spinner className="p-8 h-[6rem]" />}>
     {children}
   </Suspense>
