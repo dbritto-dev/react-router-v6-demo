5a6f89f8e9fb39fbede07d22b1afcd07f103b6df
dbritto.dev <dbritto.dev@gmail.com>
2022-08-22 10:45:41 -0500

    Update fetcher helpers

--- src/helpers/invoice-fetcher.ts
+++ src/helpers/invoice-fetcher.ts
@@ -2,6 +2,8 @@ import type { QueryFunction } from '@tanstack/react-query';
 import { Invoice } from 'models/invoices';
 import { unfetch } from './unfetch';
 
+export const getInvoiceFetcherQueryKey = (invoiceId: string) => ['invoice', invoiceId];
+
 export const invoiceFetcher =
   (invoiceId: string): QueryFunction<Invoice> =>
   async ({ signal }) =>
--- src/helpers/invoices-fetcher.ts
+++ src/helpers/invoices-fetcher.ts
@@ -2,5 +2,7 @@ import type { QueryFunction } from '@tanstack/react-query';
 import { Invoice } from 'models/invoices';
 import { unfetch } from './unfetch';
 
+export const getInvoicesFetcherQueryKey = () => ['invoices'];
+
 export const invoicesFetcher: QueryFunction<Invoice[]> = ({ signal }) =>
   unfetch('/api/invoices.json', { signal });
